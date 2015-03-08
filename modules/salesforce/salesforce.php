95e1c8c7f75c7b287bb15464de901ec56690717f
keupsonite <zakaria.lounes@gmail.com>
2015-03-08 17:03:44 +0100

    [*] Clean salesforce.php and add create table

--- modules/salesforce/salesforce.php
+++ modules/salesforce/salesforce.php
@@ -26,10 +26,10 @@
 if (!defined('_PS_VERSION_'))
 	exit;
 
+require_once('classes/SalesforceInstall.php');
+
 class Salesforce extends Module
 {
-	protected $config_form = false;
-
 	public function __construct()
 	{
 		$this->name = 'salesforce';
@@ -37,10 +37,6 @@ class Salesforce extends Module
 		$this->version = '1.0.0';
 		$this->author = 'Zakaria Lounes';
 		$this->need_instance = 0;
-
-		/**
-		 * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
-		 */
 		$this->bootstrap = true;
 
 		parent::__construct();
@@ -51,158 +47,38 @@ class Salesforce extends Module
 		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
 	}
 
-	/**
-	 * Don't forget to create update methods if needed:
-	 * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
-	 */
-	public function install()
-	{
-		Configuration::updateValue('SALESFORCE_LIVE_MODE', false);
+    public function install() {
+        if (Shop::isFeatureActive()) {
+            Shop::setContext(Shop::CONTEXT_ALL);
+        }
 
-		return parent::install() &&
-			$this->registerHook('header') &&
-			$this->registerHook('backOfficeHeader');
-	}
+        if (!parent::install() || !$this->registerHook('header') || !$this->registerHook('backofficeHeader')) {
+            return false;
+        }
 
-	public function uninstall()
-	{
-		Configuration::deleteByName('SALESFORCE_LIVE_MODE');
+        SalesforceInstall::install();
 
-		return parent::uninstall();
-	}
+        return true;
+    }
 
-	/**
-	 * Load the configuration form
-	 */
 	public function getContent()
 	{
-		/**
-		 * If values have been submitted in the form, process.
-		 */
-		$this->_postProcess();
+        if (!$this->active) {
+            return;
+        }
 
 		$this->context->smarty->assign('module_dir', $this->_path);
-
 		$output = $this->context->smarty->fetch($this->local_path.'views/templates/admin/configure.tpl');
 
-		return $output.$this->renderForm();
+		return $output;
 	}
 
-	/**
-	 * Create the form that will be displayed in the configuration of your module.
-	 */
-	protected function renderForm()
-	{
-		$helper = new HelperForm();
-
-		$helper->show_toolbar = false;
-		$helper->table = $this->table;
-		$helper->module = $this;
-		$helper->default_form_language = $this->context->language->id;
-		$helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
-
-		$helper->identifier = $this->identifier;
-		$helper->submit_action = 'submitSalesforceModule';
-		$helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
-			.'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
-		$helper->token = Tools::getAdminTokenLite('AdminModules');
-
-		$helper->tpl_vars = array(
-			'fields_value' => $this->getConfigFormValues(), /* Add values for your inputs */
-			'languages' => $this->context->controller->getLanguages(),
-			'id_language' => $this->context->language->id,
-		);
-
-		return $helper->generateForm(array($this->getConfigForm()));
-	}
-
-	/**
-	 * Create the structure of your form.
-	 */
-	protected function getConfigForm()
-	{
-		return array(
-			'form' => array(
-				'legend' => array(
-				'title' => $this->l('Settings'),
-				'icon' => 'icon-cogs',
-				),
-				'input' => array(
-					array(
-						'type' => 'switch',
-						'label' => $this->l('Live mode'),
-						'name' => 'SALESFORCE_LIVE_MODE',
-						'is_bool' => true,
-						'desc' => $this->l('Use this module in live mode'),
-						'values' => array(
-							array(
-								'id' => 'active_on',
-								'value' => true,
-								'label' => $this->l('Enabled')
-							),
-							array(
-								'id' => 'active_off',
-								'value' => false,
-								'label' => $this->l('Disabled')
-							)
-						),
-					),
-					array(
-						'col' => 3,
-						'type' => 'text',
-						'prefix' => '<i class="icon icon-envelope"></i>',
-						'desc' => $this->l('Enter a valid email address'),
-						'name' => 'SALESFORCE_ACCOUNT_EMAIL',
-						'label' => $this->l('Email'),
-					),
-					array(
-						'type' => 'password',
-						'name' => 'SALESFORCE_ACCOUNT_PASSWORD',
-						'label' => $this->l('Password'),
-					),
-				),
-				'submit' => array(
-					'title' => $this->l('Save'),
-				),
-			),
-		);
-	}
-
-	/**
-	 * Set values for the inputs.
-	 */
-	protected function getConfigFormValues()
-	{
-		return array(
-			'SALESFORCE_LIVE_MODE' => Configuration::get('SALESFORCE_LIVE_MODE', true),
-			'SALESFORCE_ACCOUNT_EMAIL' => Configuration::get('SALESFORCE_ACCOUNT_EMAIL', 'contact@prestashop.com'),
-			'SALESFORCE_ACCOUNT_PASSWORD' => Configuration::get('SALESFORCE_ACCOUNT_PASSWORD', null),
-		);
-	}
-
-	/**
-	 * Save form data.
-	 */
-	protected function _postProcess()
-	{
-		$form_values = $this->getConfigFormValues();
-
-		foreach (array_keys($form_values) as $key)
-			Configuration::updateValue($key, Tools::getValue($key));
-	}
-
-	/**
-	* Add the CSS & JavaScript files you want to be loaded in the BO.
-	*/
 	public function hookBackOfficeHeader()
 	{
 		$this->context->controller->addJS($this->_path.'js/back.js');
 		$this->context->controller->addCSS($this->_path.'css/back.css');
 	}
 
-	/**
-	 * Add the CSS & JavaScript files you want to be added on the FO.
-	 */
 	public function hookHeader()
 	{
 		$this->context->controller->addJS($this->_path.'/js/front.js');
