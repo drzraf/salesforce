a18e6b74e852a556391cc79a37033dea92030a59
Raphaël Droz <raphael.droz+floss@gmail.com>
2015-07-08 14:06:39 -0300

    more about cron

--- cron-sync-mailchimp.php
+++ cron-sync-mailchimp.php
@@ -7,11 +7,21 @@ $options = getopt("d:");
 if(isset($options['d']) && is_dir($options['d'])) {
   define('PRESTASHOP_BASEDIR', $options['d']);
 } else {
-  exit(basename(__FILE__) . " -d <DIR>"
-       . "\n\tRun the MailChimp sync of customers having pending orders."
-       . "\nParameters:\n\t-d <DIR>: The prestashop base directory"
-       . "\nEnvironment:\n\tMAILCHIMP_API_KEY (eg: de7798....-us1)"
-       . "\n\tMAILCHIMP_LIST_ID (eg: 2859...80\n");
+  exit(basename(__FILE__) .
+       <<<EOF
+ -d <DIR>
+\trun the MailChimp sync of customers having pending orders.
+
+Parameters:
+\t-d <DIR>: the prestashop base directory
+
+Environment:
+\tMAILCHIMP_API_KEY (eg: de7798....-us1)
+\tMAILCHIMP_LIST_ID (eg: 2859...80
+\tMAILCHIMP_TEST_EMAIL: if set, restrict processing to the specified email
+
+EOF
+  );
 }
 
 if(!getenv("MAILCHIMP_API_KEY")) exit("MAILCHIMP_API_KEY environment variable no set\n");
@@ -31,7 +41,12 @@ $mcApi = new \L214\MailChimp(['apiKey' => MAILCHIMP_API_KEY,
 $query = new DbQuery();
 $query->select('*')
       ->from('achats_clients_sync')
-      ->where('MCsyncEtat = "tosync" AND id_client_boutique = 4');
+  ;//->where('MCsyncEtat = "tosync"');
+
+// debugging, restrict processing to this email only
+if(getenv('MAILCHIMP_TEST_EMAIL')) {
+  $query->where('courriel = "' . getenv('MAILCHIMP_TEST_EMAIL') . '"');
+}
 
 // whereas each user may have done multiple orders waiting a sync
 // whereas MailChimp is keyed by email
@@ -43,30 +58,47 @@ foreach(Db::getInstance()->ExecuteS($query) as $o) {
   $customer = new Customer($order->id_client_boutique);
   $address = new Address(Address::getFirstCustomerAddressId($customer->id));
   $country = new Country($address->id_country);
+  $language = new Language($customer->id_lang);
   
   $email = $customer->email; // normally: same as $order->courriel
   if(isset($emails_synced[$email])) continue;
 
   $newsletter = $customer->newsletter; // normally: same as $order->newsletter
+
+  if(! $newsletter) {
+    $emails_synced[$email] = 1;
+    continue; // no more processing (no unsubscription);
+  }
+
   $cp = $address->postcode;
   $cc = $country->iso_code;
 
   $userInfo = $mcApi->getUserByEmail($email);
-  // TODO:
-  var_dump($customer); //, $userInfo);
 
-  /* useless until SalesforceEntity implements an ObjectModel
+  // var_dump($userInfo); die;
+  /* on if SalesforceEntity implements the ObjectModel
   // $order->set(...)
   $order->save(); */
 
-  $emails_synced[$email] = 1;
-  die;
-  $mcApi->subscribeNewsletter($email, $customer->firstname, $customer->$lastname,
-                              $cp, $cc, $language, 'PrestaShop', 'html', FALSE, TRUE);
+  $ret = $mcApi->subscribeNewsletter($email, $customer->firstname, $customer->lastname,
+                              $cp, $cc, $language->iso_code,
+                              'PrestaShop', 'html', FALSE, TRUE);
+  if($ret) {
+    $emails_synced[$email] = 1;
+    $msg = "mailchimp/prestashop: $email: newsletter=$newsletter";
+    if(isset($userInfo['status'])) $msg.= sprintf(' (previous status was "%s")',
+                                                  $userInfo["status"]);
+    error_log($msg);
+  }
+  else {
+    die("error during synchronisation: email=$email, id_order={$order->id_order}");
+  }
 }
 
-Db::getInstance()->update('achats_clients_sync',
-                          'MCsyncEtat = "synchronized"',
-                          'MCsyncEtat = "tosync"'
-                          . ' AND courriel IN ("' . implode('","', $emails_synced) . '")'
-                          . ' AND id_client_boutique = 4');
+if($emails_synced) {
+  Db::getInstance()->update(
+    'achats_clients_sync',
+    ['MCsyncEtat' => 'synchronized'],
+    'MCsyncEtat = "tosync"'
+    . ' AND courriel IN ("' . implode('","', array_flip($emails_synced)) . '")');
+}
